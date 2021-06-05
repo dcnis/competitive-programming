45a796a390f949369c09e9aae2adf84b16bb5b35
dcnis <bgk.dennis@yahoo.de>
2021-06-05 13:49:19 +0200

    MaxPerformanceOfTeam done

--- challenges/src/heap/MaxPerformanceOfTeam.java
+++ challenges/src/heap/MaxPerformanceOfTeam.java
@@ -1,61 +1,45 @@
 package heap;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class MaxPerformanceOfTeam {
 
-    private static int[] SPEED;
-    private static int[] EFF;
-
     public int maxPerformance(int n, int[] speed, int[] eff, int k) {
 
-        SPEED = speed;
-        EFF = eff;
+        // sort the arrays according to eff
+        int[][] effSpeed = new int[n][2];
 
-        Map<String, Long> cache = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            effSpeed[i][0] = eff[i];
+            effSpeed[i][1] = speed[i];
+        }
 
-        long result = recursion(n-1, k, 0l, Integer.MAX_VALUE, cache);
+        Arrays.sort(effSpeed, (a, b) -> b[0] - a[0]);
 
-        return (int)(result % 1000000007);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+
+        long maxResult = Long.MIN_VALUE;
+        long maxSum = 0;
+
+        for (int i = 0; i < effSpeed.length; i++) {
+
+            minHeap.add(effSpeed[i][1]);
+            maxSum += effSpeed[i][1];
+
+            if(minHeap.size() > k) {
+                maxSum -= minHeap.poll();
+            }
+
+            long currResult = (long) effSpeed[i][0] * maxSum;
+
+            maxResult = Math.max(maxResult, currResult);
+        }
+
+        return (int) (maxResult % (1e9+7));
+
     }
 
-    private long recursion(int i, int k, long sum, int minEff, Map<String, Long> cache){
-
-        if(i < 0){
-            return sum*minEff;
-        }
-
-        if(k == 0) {
-            return sum*minEff;
-        }
-
-        long maxTaken;
-        long maxNotTaken;
-
-        StringBuilder sb1 = new StringBuilder();
-        sb1.append(i-1).append("-").append(k-1).append("-").append(sum+SPEED[i]).append("-").append(Math.min(minEff, EFF[i]));
-        if(cache.containsKey(sb1.toString())){
-            maxTaken = cache.get(sb1.toString());
-        } else {
-            maxTaken = recursion(i-1, k-1, sum + SPEED[i], Math.min(minEff, EFF[i]), cache);
-            cache.put(sb1.toString(), maxTaken);
-        }
-
-        StringBuilder sb2 = new StringBuilder();
-        sb2.append(i-1).append(k).append(sum).append(minEff);
-        if(cache.containsKey(sb2.toString())){
-            maxNotTaken = cache.get(sb2.toString());
-        } else {
-            maxNotTaken = recursion(i-1, k, sum, minEff, cache);
-            cache.put(sb2.toString(), maxNotTaken);
-        }
-
-        return Math.max(maxTaken, maxNotTaken);
-
-    }
     public static void main(String[] args) {
         MaxPerformanceOfTeam program = new MaxPerformanceOfTeam();
         // should return 60
@@ -63,9 +47,11 @@ public class MaxPerformanceOfTeam {
 
 
         // should return 68
-        System.out.println(program.maxPerformance(6, new int[]{2,10,3,1,5,8}, new int[]{5,4,3,9,7,2}, 3));
+        System.out.println(program.maxPerformance(6, new int[]{2, 10, 3, 1, 5, 8}, new int[]{5, 4, 3, 9, 7, 2}, 3));
 
         // should return 72
-        System.out.println(program.maxPerformance(6, new int[]{2,10,3,1,5,8}, new int[]{5,4,3,9,7,2}, 4));
+        System.out.println(program.maxPerformance(6, new int[]{2, 10, 3, 1, 5, 8}, new int[]{5, 4, 3, 9, 7, 2}, 4));
     }
+
+
 }
