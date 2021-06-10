cdb249df8ee934d37c85e861a65ff607d7c75ee5
dcnis <bgk.dennis@yahoo.de>
2021-06-10 20:35:26 +0200

    finished optimal solution

--- challenges/src/heap/MyCalenderI.java
+++ challenges/src/heap/MyCalenderI.java
@@ -1,51 +1,26 @@
 package heap;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Set;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 public class MyCalenderI {
 
-    TreeSet<Integer[]> treeSet = new TreeSet<>((a, b) -> a[0]-b[0]);
+    TreeSet<int[]> treeSet = new TreeSet<>((a, b) -> a[0]-b[0]);
 
     public boolean book(int start, int end) {
 
-        Integer[] entry = new Integer[]{start, end};
-        boolean couldAdd = treeSet.add(entry);
-
-        if(!couldAdd) return false;
-
-        if(treeSet.size() == 0){
-            treeSet.add(entry);
-            return true;
-        }
+        int[] entry = new int[]{start, end};
 
         // getLower and check
-        Integer[] lower = treeSet.lower(entry);
-        if(lower != null){
-            // do Check
-            if(entry[0] < lower[1] && lower[0] < entry[1]){
-                treeSet.remove(entry);
-                return false;
-            }
+        int[] lowerOrSame = treeSet.ceiling(entry);
+        if(lowerOrSame != null && entry[0] < lowerOrSame[1] && lowerOrSame[0] < entry[1]){
+            return false;
         }
 
-        Integer[] higher = treeSet.higher(entry);
-        if(higher != null){
-            // do check
-            if(entry[0] < higher[1] && higher[0] < entry[1]){
-                treeSet.remove(entry);
-                return false;
-            }
+        int[] higherOrSame = treeSet.floor(entry);
+        if(higherOrSame != null && entry[0] < higherOrSame[1] && higherOrSame[0] < entry[1]){
+            return false;
         }
-
-
-
+        treeSet.add(entry);
 
         return true;
 
