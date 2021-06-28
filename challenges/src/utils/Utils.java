be95ff0e293da63762520efdc6f4503706bbadc8
dcnis <bgk.dennis@yahoo.de>
2021-06-28 21:33:19 +0200

    added 2DArray Util

--- challenges/src/utils/Utils.java
+++ challenges/src/utils/Utils.java
@@ -13,8 +13,24 @@ public class Utils {
         return array;
     }
 
+    public int[][] read2DArray(String twoDarray){
+        String removeBrackets = twoDarray.substring(1, twoDarray.length()-1);
+        String[] splitted = removeBrackets.split("]");
+        int[][] result = new int[splitted.length][];
+
+        for(int i = 0; i < splitted.length; i++){
+            splitted[i] = splitted[i].replace(",[", "[");
+            splitted[i] += "]";
+            result[i] = this.readArray(splitted[i]);
+
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Utils utils = new Utils();
         utils.readArray("[8, 5, 2, 9, 5, 6, 3]");
+        utils.read2DArray("[[1,3,5,7],[10,11,16,20],[23,30,34,60]]");
     }
 }
