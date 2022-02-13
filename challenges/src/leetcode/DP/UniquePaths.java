071f9a2233e4b485200c6695d053f4134ae72939
dcnis <bgk.dennis@yahoo.de>
2022-02-13 20:51:41 +0100

    added DP Problem UniquePaths

---
+++ challenges/src/leetcode/DP/UniquePaths.java
@@ -0,0 +1,26 @@
+package leetcode.DP;
+
+/*
+* 62. Unique Paths
+* https://leetcode.com/problems/unique-paths/
+* */
+public class UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        int[][] memo = new int[m+1][n+1];
+        return dp(m, n, memo);
+    }
+
+    private int dp(int rows, int columns, int[][] memo){
+
+        if(rows == 1 && columns == 1) return 1;
+        if(rows == 0 || columns == 0) return 0;
+
+        if(memo[rows][columns] != 0) return memo[rows][columns];
+
+        int answer = dp(rows-1, columns, memo) + dp(rows, columns-1, memo);
+        memo[rows][columns] = answer;
+        return answer;
+    }
+
+}
