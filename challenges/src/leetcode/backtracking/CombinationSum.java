4ff39e33d37cc678ab20909db5c425bfc91e576c
schmidtdennissasch <schmidtdennissasch@cainiao.com>
2022-10-05 13:03:08 +0800

    Solved CombinationSum

---
+++ challenges/src/leetcode/backtracking/CombinationSum.java
@@ -0,0 +1,40 @@
+package leetcode.backtracking;
+
+import java.util.*;
+
+/**
+ * 39. Combination Sum
+ * https://leetcode.com/problems/combination-sum/
+ */
+public class CombinationSum {
+
+    public List<List<Integer>> combinationSum(int[] can, int target) {
+
+        Set<List<Integer>> solution = new HashSet<>();
+        LinkedList<Integer> cur = new LinkedList<>();
+        backtrack(0, cur, 0, solution, can, target);
+        return new ArrayList<>(solution);
+    }
+
+
+    private void backtrack(int i, LinkedList<Integer> cur, int sum, Set<List<Integer>> solution, int[] can, int target){
+
+
+        if(sum == target){
+            solution.add(new ArrayList<>(cur));
+            return;
+        }
+
+        if(sum > target) return;
+        if(i > can.length-1) return;
+
+        cur.offer(can[i]);
+        backtrack(i+1, cur, sum+can[i], solution, can, target);
+        backtrack(i, cur, sum+can[i], solution, can, target);
+        cur.removeLast();
+
+        backtrack(i+1, cur, sum, solution, can, target);
+
+    }
+
+}
